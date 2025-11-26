38da473c327d96688f65b61ad14333d8dd96afe5
Nicolas Garric <nicolasdfgarric@gmail.com>
2025-11-26 09:45:52 +0100

    Plugin installation

--- config/application.php
+++ config/application.php
@@ -142,8 +142,13 @@ Config::define('CONCATENATE_SCRIPTS', false);
  */
 Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', false);
+
 Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', '0');
+
+// Accept installation directly from the WP plugin page
+Config::define('FS_METHOD', 'direct');
+
 
 /**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
