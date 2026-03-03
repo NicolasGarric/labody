0255e47fd732917f1552d1f3c569f5a5b8f18e37
Nicolas Garric <nicolasdfgarric@gmail.com>
2026-03-03 17:10:13 +0100

    Adding color switch regarding background for heading links

--- web/app/themes/localdy/views/pages/homepage.js
+++ web/app/themes/localdy/views/pages/homepage.js
@@ -16,3 +16,24 @@ document.addEventListener("DOMContentLoaded", () => {
         target.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     });
 });
+
+
+// Header Black/White switch - base.twig & base.scss
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".header");
+    const hero = document.querySelector(".homepage__heading");
+
+    if (!header || !hero) return;
+
+    const heroHeight = hero.offsetHeight;
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > heroHeight - 80) {
+            header.classList.remove("header--light");
+            header.classList.add("header--dark");
+        } else {
+            header.classList.remove("header--dark");
+            header.classList.add("header--light");
+        }
+    });
+});
